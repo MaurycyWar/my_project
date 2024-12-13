d41b78d70ffaad2b5ea994e7aacec0d5e2fd0230
Maurycy Warszewski <maurycy2013@gmail.com>
2024-12-13 09:38:51 +0000

    skibidi

--- src/my_project_frontend/src/App.vue
+++ src/my_project_frontend/src/App.vue
@@ -1,21 +1,31 @@
 <script setup>
 import { ref } from 'vue';
 import { my_project_backend } from 'declarations/my_project_backend/index';
-let greeting = ref('');
+// let greeting = ref('');
 
-async function handleSubmit(e) {
-  e.preventDefault();
-  const target = e.target;
-  const name = target.querySelector('#name').value;
-  await my_project_backend.greet(name).then((response) => {
-    greeting.value = response;
-  });
+const getDataFromNBP = async () => {
+    const res = await fetch("https://api.nbp.pl/api/exchangerates/tables/A/?format=json");
+    const jsonData = await res.json();
+    console.log(jsonData);
+    const rates = jsonData[0].rates;
+    console.log(rates);
 }
+getDataFromNBP();
+
+// async function handleSubmit(e) {
+//   e.preventDefault();
+//   const target = e.target;
+//   const name = target.querySelector('#name').value;
+//   await my_project_backend.greet(name).then((response) => {
+//     greeting.value = response;
+//   });
+// }
 </script>
 
 <template>
-  <main>
-    <img src="/logo2.svg" alt="DFINITY logo" />
+    <main>
+
+        <!-- <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
     <br />
     <form action="#" @submit="handleSubmit">
@@ -23,6 +33,6 @@ async function handleSubmit(e) {
       <input id="name" alt="Name" type="text" />
       <button type="submit">Click Me!</button>
     </form>
-    <section id="greeting">{{ greeting }}</section>
-  </main>
+    <section id="greeting">{{ greeting }}</section> -->
+    </main>
 </template>
